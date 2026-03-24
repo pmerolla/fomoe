b464eaf86e1258ed507c7346d77fe78aef101ceb
Jonny Leaders <jonathan.leaders@classdojo.com>
2026-03-24 16:16:08 -0500

    Add native CUDA BF16 matvec support

--- include/gpu_compat.h
+++ include/gpu_compat.h
@@ -15,6 +15,7 @@
 // ============================================================
 #include <cuda_runtime.h>
 #include <cuda_fp16.h>
+#include <cuda_bf16.h>
 
 // --- Types ---
 #define hipStream_t             cudaStream_t
